9863f5f84c289831c49cf5ffa80f4b1baf900939
Junaid Abdul Rahman <105756455+Junaid-12345@users.noreply.github.com>
2023-06-16 01:08:59 +0100

    Create booking.js
    booking component

---
+++ booking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookingForm from './BookingForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Restaurant Booking System</h1>
+      <BookingForm />
+    </div>
+  );
+};
+
+export default App;
